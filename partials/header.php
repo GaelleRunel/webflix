6c3b27b743dd22e393c621ddae7e311b4ec35d93
Gaelle <gaelle.runel@gmail.com>
2018-10-29 15:57:54 +0100

    Create webflix structure

---
+++ partials/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Webflix</title>
+  </head>
+
+ <!-- ======================= NAVBAR =========================== -->
+    
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Webflix</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liste des films</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ajouter un film</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
