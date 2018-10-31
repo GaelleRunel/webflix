3ad5191c83ec2f0f8b6ff08792494d6c6e4b4e8b
Gaelle <gaelle.runel@gmail.com>
2018-10-31 11:59:45 +0100

    Create edit and delete pages

---
+++ movie_delete.php
@@ -0,0 +1,16 @@
+<!-- Le fichier header.php est inclus dans la page -->
+<?php require_once(__DIR__.'/partials/header.php'); ?>
+
+     <!-- ======================= MAIN =========================== -->
+
+<main>
+    <h1 class="page-title"><i class="fab fa-hotjar"></i> Suppression d'un film</h1>
+     
+
+</main>
+
+    <!-- ======================= SCRIPTS =========================== -->
+
+<!-- Le fichier footer.php est inclus dans la page -->
+<?php require_once(__DIR__.'/partials/footer.php'); ?>
+   
